258db3370ba00d2cb754cdafa773c65ccd6732fe
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-04 10:08:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,6 @@ def oxford_comma(array)
     when 2 
       return array.join(" and ")
     when 3
-      return array[0...-1].join(' , ') + " and " + array[-1]
+      return array[0...-1].join(', ') + ", and " + array[-1]
   end
 end
