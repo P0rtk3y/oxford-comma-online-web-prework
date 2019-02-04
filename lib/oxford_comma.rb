1b93138dcc9e4723a2c6fea42d8459f29b0f1e5d
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-04 10:09:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,8 @@ def oxford_comma(array)
     when 2 
       return array.join(" and ")
     when 3
-      return array[0...-1].join(', ') + ", and " + array[-1]
+      return array[0...2].join(', ') + ", and " + array[2]
+    else
+      return array
   end
 end
